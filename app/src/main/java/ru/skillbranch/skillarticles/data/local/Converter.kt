ab89b556f24f3b0777753f40cbdbaae5a8496a18
Anton Mamkin <amamkin@ourgapps.com>
2020-08-18 17:59:18 +0300

    daos

---
+++ app/src/main/java/ru/skillbranch/skillarticles/data/local/Converter.kt
@@ -0,0 +1,17 @@
+package ru.skillbranch.skillarticles.data.local
+
+import androidx.room.TypeConverter
+import java.util.*
+
+class DateConverter {
+    @TypeConverter
+    fun timestampToDate(timestamp: Long): Date = Date(timestamp)
+
+    @TypeConverter
+    fun dateToTimestamp(date: Date): Long = date.time
+}
+
+class MarkdownConverter {
+    @TypeConverter
+    fun toMarkdown(timestamp: Long): Date = Date(timestamp)
+}

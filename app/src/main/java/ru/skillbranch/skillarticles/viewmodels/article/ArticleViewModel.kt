3fc9f30f1e646b91f540f292b5f1046490767aaa
Anton Mamkin <amamkin@ourgapps.com>
2020-09-10 17:42:43 +0300

    9

--- app/src/main/java/ru/skillbranch/skillarticles/viewmodels/article/ArticleViewModel.kt
+++ app/src/main/java/ru/skillbranch/skillarticles/viewmodels/article/ArticleViewModel.kt
@@ -54,7 +54,9 @@ class ArticleViewModel(
                 isBookmark = article.isBookmark,
                 isLike = article.isLike,
                 content = article.content ?: emptyList(),
-                isLoadingContent = article.content == null
+                isLoadingContent = article.content == null,
+                hashtags = article.tags,
+                source = article.source
 
             )
         }
@@ -252,7 +254,9 @@ data class ArticleState(
     val answerTo: String? = null,
     val answerToSlug: String? = null,
     val showBottomBar: Boolean = true,
-    val commentText: String? = null
+    val commentText: String? = null,
+    val hashtags: List<String> = emptyList(),
+    val source: String? = null
 
 ) : IViewModelState {
     override fun save(outState: SavedStateHandle) {
